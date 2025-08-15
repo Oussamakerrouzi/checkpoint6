16155877969039bc848bd2fab0b44056a10fdb5c
Oussamakerrouzi <oussakerr.business@gmail.com>
2025-08-15 18:31:30 +0100

    Create After Code (Reusable).js

---
+++ After Code (Reusable).js
@@ -0,0 +1,2 @@
+container.appendChild(createCard("Basic Plan", "$9.99 / month", ["1 GB Storage", "Basic Support", "All Core Features"]));
+container.appendChild(createCard("Pro Plan", "$19.99 / month", ["10 GB Storage", "Priority Support", "Advanced Features"]));
